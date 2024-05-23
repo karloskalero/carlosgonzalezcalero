854d96c4136950d9d51aca7f8d280afbd5e204ca
karloskalero <77688635+karloskalero@users.noreply.github.com>
2024-05-23 13:34:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, render_template  # Agregar render_template
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 # Ruta del archivo Excel
 excel_file = 'data/datos.xlsx'
